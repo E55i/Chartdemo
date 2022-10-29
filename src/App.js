4b050076d625be168456d3f0fb767aeba3174940
Lasse Haverinen <lasse.haverinen@gmail.com>
2022-10-29 11:36:34 +0300

    Added basic demo of react-chartjs usage with line chart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { Chart } from "chart.js/auto";
+import { Line } from "react-chartjs-2";
+import co2data from "./data/Co2-demo.json";
 
 function App() {
+  /*let co2DataPadded = [...co2data];
+  let padding = [];
+  for (let i = co2data.length; i < temperatureData.length; i++) {
+    co2DataPadded.push({
+      TimeYrBP: (
+        parseInt(temperatureData[i].kYearsFromPresent) * 1000
+      ).toString(),
+    });
+  }*/
+
+  const data = {
+    datasets: [
+      {
+        label: "Co2 ppm",
+        data: [...co2data].reverse(),
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+        yAxisID: "co2",
+        parsing: {
+          xAxisKey: "TimeYrBP",
+          yAxisKey: "Co2ppm",
+        },
+        pointRadius: 1,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Demo Co2 plot",
+      },
+    },
+    scales: {
+      co2: {
+        type: "linear",
+        display: true,
+        position: "right",
+      },
+    },
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ width: "1000px" }}>
+        <Line options={options} data={data} />
+      </div>
     </div>
   );
 }
